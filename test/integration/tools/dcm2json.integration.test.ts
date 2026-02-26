214d0ffea94d8ee9717af3aae10fa1ba0c883775
Michael Hobbs <michael.lee.hobbs@gmail.com>
2026-02-26 16:59:55 -0500

    Refactor dcm2json import to use barrel import from src

--- test/integration/tools/dcm2json.integration.test.ts
+++ test/integration/tools/dcm2json.integration.test.ts
@@ -1,5 +1,5 @@
-import { describe, it, expect } from 'vitest';
-import { dcm2json } from '../../../src/tools/dcm2json';
+import { describe, expect, it } from 'vitest';
+import { dcm2json } from '../../../src';
 import { dcmtkAvailable, SAMPLES } from '../helpers';
 
 describe.skipIf(!dcmtkAvailable)('dcm2json integration', () => {
